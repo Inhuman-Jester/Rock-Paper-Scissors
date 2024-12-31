4b0f5a7a2c1d1df839607de9b75e176b3fda29ee
Inhuman <hj29official@gmail.com>
2024-12-31 22:21:59 +0530

    First draft of successfully implemented rock-paper-scissors

--- script.js
+++ script.js
@@ -5,45 +5,92 @@ function getCompChoice(){
     if(a==1)    return "paper";
 }
 
-function getHumanChoice(){
-    let a=prompt("What is your move? \n");
-    return a.toLowerCase();
-}
+//select buttons
+// let rock= document.querySelector("#btn1")
+// let paper= document.querySelector("#btn2")
+// let scissors= document.querySelector("#btn3")
 
+// let h=document.querySelector("btn");
+// h.addEventListener("click", (event)=>{
+//     return event.target.id
+// })
+
+let roundNumber=0
+
+let h=document.querySelector("#btn");
+
+h.addEventListener("click", (event)=>{
+    if(event.target.tagName==="BUTTON"){
+        humanChoice=event.target.id;
+        roundNumber++;
+        playRound(humanChoice);
+    }
+})
+
+//declare the scoring variables
 let humanScore=0
 let compScore=0
 
-function playRound(n){
-    if(n==0)    return;
-
-    let human=getHumanChoice();
-    let comp=getCompChoice();
-
-    console.log(`Comp move ${comp}\n`);
+//function on how the round shall be played using recursion
+function playRound(humanChoice){
     
+    let compChoice=getCompChoice();
+    
+    console.log(`Round ${roundNumber}`);
 
-    check(human, comp);
-    console.log(`Score: Comp-${compScore}   Human-${humanScore}`)
+    check(humanChoice, compChoice);
+    
+    console.log(`Computer: ${compScore}     Human:${humanScore}`)
 
-    playRound(n-1);
+    if(compScore>humanScore&&compScore==5){
+        console.log("Computer Wins!");
+        humanScore=0;
+        compScore=0;
+        roundNumber=0;
+    }  
+    else if(compScore<humanScore&&humanScore==5){
+        console.log("Human Wins!");
+        humanScore=0;
+        compScore=0;
+        roundNumber=0;
+    }    
+}
+
+//function which checks who wins the point
+function check(humanChoice, compChoice){
+    if(humanChoice=="rock"){
+        if(compChoice=="scissors"){
+            humanScore++;
+            console.log("Human Wins this round")
+        }    
+        else if(compChoice=="paper"){
+            compScore++;
+            console.log("Computer wins this round")
+        }   
+    }
+    else if(humanChoice=="paper"){
+        if(compChoice=="scissors"){
+            compScore++;
+            console.log("Computer wins this round")
+        }    
+        else if(compChoice=="rock"){
+            console.log("Human Wins this round")
+            humanScore++;
+        }    
+    }
+    else if(humanChoice=="scissors"){
+        if(compChoice=="paper"){
+            console.log("Human Wins this round")
+            humanScore++;
+        }
+        else if(compChoice=="rock"){
+            compScore++;
+            console.log("Computer wins this round")
+        }
+    }
     return;
 }
 
-function check(human, comp){
-    if(human=="rock"){
-        if(comp=="scissors")    humanScore++;
-        else if(comp=="paper")   compScore++;
-    }
-    else if(human=="paper"){
-        if(comp=="scissors")    compScore++;
-        else if(comp=="rock")    humanScore++;
-    }
-    else if(human=="scissors"){
-        if(comp=="paper")   humanScore++;
-        else if(comp=="rock")    compScore++;
-    }
-}
 
-playRound(5);
-if(compScore>humanScore)    console.log("Comp Wins!");
-else    console.log("Human Wins");
+
+// final output
