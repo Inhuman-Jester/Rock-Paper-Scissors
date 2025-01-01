813342ccc07ef17ed2fcd68ce1ab60e56ca3f2d4
Inhuman <hj29official@gmail.com>
2025-01-01 11:20:43 +0530

    first draft of version with some ui

--- script.js
+++ script.js
@@ -5,23 +5,13 @@ function getCompChoice(){
     if(a==1)    return "paper";
 }
 
-//select buttons
-// let rock= document.querySelector("#btn1")
-// let paper= document.querySelector("#btn2")
-// let scissors= document.querySelector("#btn3")
-
-// let h=document.querySelector("btn");
-// h.addEventListener("click", (event)=>{
-//     return event.target.id
-// })
-
 let roundNumber=0
 
 let h=document.querySelector("#btn");
 
 h.addEventListener("click", (event)=>{
     if(event.target.tagName==="BUTTON"){
-        humanChoice=event.target.id;
+        let humanChoice=event.target.id;
         roundNumber++;
         playRound(humanChoice);
     }
@@ -35,24 +25,27 @@ let compScore=0
 function playRound(humanChoice){
     
     let compChoice=getCompChoice();
-    
+
     console.log(`Round ${roundNumber}`);
 
     check(humanChoice, compChoice);
     
     console.log(`Computer: ${compScore}     Human:${humanScore}`)
+    displayScore(`Computer: ${compScore}     Human:${humanScore}`)
 
     if(compScore>humanScore&&compScore==5){
         console.log("Computer Wins!");
         humanScore=0;
         compScore=0;
         roundNumber=0;
+        // displayScore("Computer Wins!");
     }  
     else if(compScore<humanScore&&humanScore==5){
         console.log("Human Wins!");
         humanScore=0;
         compScore=0;
         roundNumber=0;
+        // displayScore("Human Wins!");
     }    
 }
 
@@ -91,6 +84,10 @@ function check(humanChoice, compChoice){
     return;
 }
 
-
-
-// final output
+//function to display stuff on the screen 
+function displayScore(string){
+    let addition=document.createElement("h2");
+    addition.innerText=string;
+    // addition.style.alignSelf="center";
+    h.appendChild(addition);
+}
